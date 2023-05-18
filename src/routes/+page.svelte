32d3c00ba06fa4760682f8e4a4985a45a427b393
Elara Musayelyan <elara@elara.ws>
2023-05-17 22:17:04 -0700

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Header from './Header.svelte';
+	import Icon from '@iconify/svelte';
+	import tippy from 'svelte-tippy';
+	import 'tippy.js/dist/tippy.css';
+
+	let tooltipContent = 'Click to copy';
+
+	function copyEmail() {
+		navigator.clipboard.writeText('email@example.com');
+		tooltipContent = 'Copied!';
+		setTimeout(() => {
+			tooltipContent = 'Click to copy';
+		}, 2000);
+	}
+</script>
+
+<Header />
+
+<section class="hero is-link is-fullheight-with-navbar">
+	<div class="hero-body">
+		<div class="container has-text-centered">
+			<p class="title">Conner Chu</p>
+		</div>
+	</div>
+	<div class="hero-foot">
+		<div class="container has-text-centered mb-3 is-size-3">
+			<a href="https://github.com/cchuster"><Icon icon="mdi:github" /></a>
+			<a href="https://www.linkedin.com/in/connerchu"><Icon icon="mdi:linkedin" /></a>
+			<a
+				href="javascript:void(0);"
+				use:tippy={{ content: tooltipContent, placement: 'top' }}
+				on:click={copyEmail}
+			>
+				<Icon icon="material-symbols:mail" />
+			</a>
+		</div>
+	</div>
+</section>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Icon from '@iconify/svelte';
+
+	let navbarActive = false;
+
+	function toggleNavbar() {
+		navbarActive = !navbarActive;
+	}
+</script>
+
+<nav class="navbar">
+	<div class="container">
+		<div class="navbar-brand is-size-3">
+			<a class="navbar-item x" href="/">
+				<Icon icon="material-symbols:house" />
+			</a>
+			<span class="navbar-burger {navbarActive ? 'is-active' : ''}" on:click={toggleNavbar}>
+				<span />
+				<span />
+				<span />
+			</span>
+		</div>
+		<div class="navbar-menu {navbarActive ? 'is-active' : ''} is-size-4-desktop">
+			<div class="navbar-start mx-auto">
+				<a href="/research" class="navbar-item mr-60-desktop">Research</a>
+				<a href="/resume" class="navbar-item mr-60-desktop">Resume</a>
+				<a href="/projects" class="navbar-item">Projects</a>
+			</div>
+			<a class="navbar-item"><Icon icon="bi:translate" /></a>
+		</div>
+	</div>
+</nav>
