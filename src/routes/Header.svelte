339d2db751bc40e57b96559b9344e1488453fc14
Elara Musayelyan <elara@elara.ws>
2023-05-18 18:51:23 -0700

    Move icons to the edge of the navbar

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -15,26 +15,24 @@
 <svelte:window bind:innerWidth={windowWidth} />
 
 <nav class="navbar">
-	<div class="container">
-		<div class="navbar-brand is-size-3">
-			<a class="navbar-item x" href="/">
-				<Icon icon="material-symbols:house" />
-			</a>
-			<span class="navbar-burger {navbarActive ? 'is-active' : ''}" on:click={toggleNavbar}>
-				<span />
-				<span />
-				<span />
-			</span>
-		</div>
-		{#if navbarActive || windowWidth >= desktopSize}
-			<div transition:slide class="navbar-menu is-active is-size-4-desktop">
-				<div class="navbar-start mx-auto">
-					<a href="/research" class="navbar-item mr-60-desktop">Research</a>
-					<a href="/resume" class="navbar-item mr-60-desktop">Resume</a>
-					<a href="/projects" class="navbar-item">Projects</a>
-				</div>
-				<a class="navbar-item"><Icon icon="bi:translate" /></a>
-			</div>
-		{/if}
+	<div class="navbar-brand is-size-3">
+		<a class="navbar-item x" href="/">
+			<Icon icon="material-symbols:house" />
+		</a>
+		<span class="navbar-burger {navbarActive ? 'is-active' : ''}" on:click={toggleNavbar}>
+			<span />
+			<span />
+			<span />
+		</span>
 	</div>
+	{#if navbarActive || windowWidth >= desktopSize}
+		<div transition:slide class="navbar-menu is-active is-size-4-desktop">
+			<div class="navbar-start mx-auto">
+				<a href="/research" class="navbar-item mr-60-desktop">Research</a>
+				<a href="/resume" class="navbar-item mr-60-desktop">Resume</a>
+				<a href="/projects" class="navbar-item">Projects</a>
+			</div>
+			<a class="navbar-item"><Icon icon="bi:translate" /></a>
+		</div>
+	{/if}
 </nav>
