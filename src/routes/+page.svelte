bccbfea24ba2420fe577ce6f49c7d396242f259f
Conner Chu <creativechu@outlook.com>
2023-05-21 11:44:11 -0700

    Finish home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,22 +17,28 @@
 	}
 </script>
 
+<svelte:head>
+    <style>html,body {overflow-y:hidden; height:100%}</style>
+</svelte:head>
+
 <Header />
 
-<section class="hero is-link is-fullheight-with-navbar">
+<section class="hero is-link is-fullheight-with-navbar" style="background-color: #1A1A1A;">
 	<div class="hero-body">
 		<OnMount>
 			<div class="container has-text-centered" in:fade>
-				<p class="title">Conner Chu</p>
+				<p class="title" style="font-size: 5.5vh;">Conner Chu</p>
+				<p class="title" style="font-size: 3.5vh;">MCB @ Berkeley</p>
 			</div>
 		</OnMount>
 	</div>
 	<div class="hero-foot">
 		<div class="container has-text-centered mb-3 is-size-3">
-			<a href="https://github.com/cchuster"><Icon icon="mdi:github" /></a>
-			<a href="https://www.linkedin.com/in/connerchu"><Icon icon="mdi:linkedin" /></a>
+			<a href="https://github.com/cchuster" style="font-size: 4vh;"><Icon icon="mdi:github" /></a>
+			<a href="https://www.linkedin.com/in/connerchu" style="font-size: 4vh;"><Icon icon="mdi:linkedin" /></a>
 			<a
 				href="javascript:void(0);"
+				style="font-size: 4vh;"
 				use:tippy={{ content: tooltipContent, placement: 'top' }}
 				on:click={copyEmail}
 			>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -26,9 +26,14 @@
 
 <svelte:window bind:innerWidth={windowWidth} />
 
-<nav class="navbar">
+<nav class="navbar is-dark" style="background-color: #1A1A1A;">
 	<div class="navbar-brand is-size-3">
-		<a class="navbar-item is-hidden-desktop" href="/"><Icon icon="material-symbols:house" /></a>
+		<a
+			class="navbar-item is-hidden-desktop"
+			href="/"
+			style="font-size: 3vw; color:white; margin-left: .5%; margin-top: .5%"
+			><Icon icon="fa6-solid:house-chimney" /></a
+		>
 		<span class="navbar-burger {navbarActive ? 'is-active' : ''}" on:click={toggleNavbar}>
 			<span />
 			<span />
@@ -39,15 +44,40 @@
 		<div
 			transition:slideBurger
 			class="navbar-menu {navbarActive ? 'is-active' : ''} is-size-4-desktop"
+			style="background-color: #1A1A1A;"
 		>
-			<a class="navbar-item is-hidden-touch" href="/"><Icon icon="material-symbols:house" /></a>
+			<a
+				class="navbar-item is-hidden-touch"
+				href="/"
+				style="font-size: 3.5vh; color:white; margin-left: .5%; margin-top: .5%"
+				><Icon icon="fa6-solid:house-chimney" /></a
+			>
 			<div class="navbar-start mx-auto">
-				<a href="/research" class="navbar-item mr-60-desktop">Research</a>
-				<a href="/resume" class="navbar-item mr-60-desktop">Resume</a>
-				<a href="/projects" class="navbar-item">Projects</a>
+				<a
+					href="/research"
+					class="navbar-item {navbarActive ? 'is-burger-active' : ''} mr-60-desktop"
+					style="color:white; font-size: {navbarActive ? '2vh' : '3vh'}; margin-top: .5%"
+					>Research</a
+				>
+				<a
+					href="/resume"
+					class="navbar-item {navbarActive ? 'is-burger-active' : ''} mr-60-desktop"
+					style="color:white; font-size: {navbarActive ? '2vh' : '3vh'}; margin-top: .5%"
+					>Resume</a
+				>
+				<a
+					href="/projects"
+					class="navbar-item {navbarActive ? 'is-burger-active' : ''}"
+					style="color:white; font-size: {navbarActive ? '2vh' : '3vh'}; margin-top: .5%"
+					>Projects</a
+				>
 			</div>
-			<a class="navbar-item is-hidden-touch"><Icon icon="bi:translate" /></a>
-			<a class="navbar-item is-hidden-desktop">Translate</a>
+			<a
+				class="navbar-item is-hidden-touch"
+				style="font-size: 3.5vh; color:white; margin-right:.5%; margin-top: .5%"
+				><Icon icon="bi:translate" /></a
+			>
+			<a class="navbar-item is-hidden-desktop" style="font-size:2vh; color:white;">English | 中文</a>
 		</div>
 	{/if}
 </nav>
