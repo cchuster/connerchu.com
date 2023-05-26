1bd6c0efc263146e3971bc27563597543eeba50b
Elara Musayelyan <elara@elara.ws>
2023-05-26 09:23:29 -0700

    Add PDF reader to resume page

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -3,3 +3,13 @@
 </script>
 
 <Header />
+
+<section class="hero is-dark">
+	<div class="hero-body">
+		<div class="container box has-shadow">
+			<object type="application/pdf"
+					    data="/Basic_Resume.docx.pdf" style="width: 100%; height: 100vh;">
+			</object>
+		</div>
+	</div>
+</section>
